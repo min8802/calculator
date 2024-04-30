eb1b9b0ad4d12aa0fe548de5b7e5cf85458c88ec
min8802 <ww330@naver.com>
2024-04-30 13:50:04 +0900

    onclick number

--- main.js
+++ main.js
@@ -2,4 +2,25 @@ let result = 0;
 
 const resultEl = document.querySelector(".result");
 
+
 resultEl.innerHTML = result;
+
+//0이면 number 값으로 교체, 그것이 아니면 원래 숫자에 더하기
+//23 글자가 넘어가지면 입력이 안되게 -> 23글자 넘어가면 함수를 끝내버리자 이런패턴이 많이쓰임
+//return은 값을 반환하고 '함수를 끝낸다는 의미'가 있다 -> return만 하면 함수끝난다!!
+//값을 반환안하고 return만 하면 함수만 끝난다는 의미임
+// return 밖에 없을 때는 코드 블록을 없애도 똑같이 동작한다 ! 오 ~~
+//어떤 조건이 충족되지 않으면 함수를 끝낸다는 개념 많이 사용
+
+function onClickNumber(number) {
+    if (resultEl.innerHTML.length >= 22) return;
+    
+
+    if (resultEl.innerHTML === "0") {
+      resultEl.innerHTML = number;
+    } else {
+      resultEl.innerHTML += number;
+    }
+
+    console.log(resultEl.innerHTML.length)
+  }
